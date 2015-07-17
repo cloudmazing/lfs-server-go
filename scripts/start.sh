bff41903b9fcfa047435fda258574fded6b8d519
Mike Quinn <memikequinn@gmail.com>
2015-07-17 08:37:01 -0500

    add ldap server start when dev

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,5 +6,8 @@ set -o pipefail
 cd $(dirname $0)/../
 GO_ENV=${GO_ENV:-dev}
 export GO_ENV
+
+[[ "${GO_ENV}" == "dev" ]] && [[ ! "`ps -ef | grep '[t]est_ldap'`" ]] && ./test_ldap/test_ldap
+
 [[ ! -f "lfs-server-go" ]] && go build
 ./lfs-server-go
