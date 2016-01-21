132fc9e00cddce3d5834621faa21bb41aab94444
Mike Quinn <memikequinn@gmail.com>
2016-01-20 23:15:34 -0800

    fix go vet issues

--- user_service.go
+++ user_service.go
@@ -70,8 +70,7 @@ func (us *UserService) GetResponse() error {
 		us.UserAccessResponse.Filled = true
 		return nil
 	} else {
-		errors.New(fmt.Sprintf("STATUS ERR: %s", err.Error()))
-		return err
+		return errors.New(fmt.Sprintf("STATUS ERR: %s", err.Error()))
 	}
 
 }
